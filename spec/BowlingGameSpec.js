4d75cc0146d10fd8ffeb7039574d2d0f869ac955
Alain Noel <alain.noel34@gmail.com>
2022-09-06 14:48:49 +0200

    test en cours

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,16 @@
+describe("Bowling game", function() {
+  it("should properly calculate a gutter game", function(){
+    rollMany(20, 0);
+    expect(game.score()).toEqual(0);
+        // tests
+  });
+
+  it("should properly calculate a strike", function (){
+    //test
+  });
+
+  it("should properly calculate a square", function(){
+    //test
+  });
+
+});
