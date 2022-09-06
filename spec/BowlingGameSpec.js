6765c7a4880c91e258246806aa4fc8d514a7f094
Alain Noel <alain.noel34@gmail.com>
2022-09-06 17:30:25 +0200

    test gutter game ok

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,16 +1,26 @@
-describe("Bowling game", function() {
-  it("should properly calculate a gutter game", function(){
-    rollMany(20, 0);
-    expect(game.score()).toEqual(0);
-        // tests
+describe("BowlingGame", function() {
+  var game;
+
+  beforeEach(function(){
+      game = new BowlingGame();
   });
 
-  it("should properly calculate a strike", function (){
-    //test
+  function rollMany(n, pins) {
+    for (var i = 0; i < n; i++) {
+      game.roll(pins)
+    }
+  }
+  it("should handle a gutter game", function() {
+      rollMany(20, 0);
+      expect(game.score()).toEqual(0);
   });
 
-  it("should properly calculate a square", function(){
-    //test
+  it("should properly calculate a strike", function () {
+      // test
+  });
+
+  it("should properly calculate a spare", function () {
+      // test
   });
 
 });
